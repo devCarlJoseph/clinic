da241481e3a1571b0d0c5f13cc28ff793f3ceef7
devCarlJoseph <carljosephsumagang17@gmail.com>
2026-09-18 13:47:27 +0800

    feat: add Receptionist class with inheritance

--- app/Models/Receptionist.php
+++ app/Models/Receptionist.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "this is my receptionist";
+class Receptionist extends DentalClinicUser
+{
+    protected string $employeeNo;
+    protected string $shift;
+
+    public function __construct(string $userId, string $name, string $contactNum, string $employeeNo, string $shift)
+    {
+        parent::__construct($userId, $name, $contactNum);
+
+        $this->employeeNo = $employeeNo;
+        $this->shift = $shift;
+    }
+
+    public function getRoleDescription(): string 
+    {
+        return "Receptionist - handles registration and appointment assistance.";
+    }
+}
