251a94f647b8bf459992f492ba4456fcaa5e61e4
devCarlJoseph <carljosephsumagang17@gmail.com>
2026-09-18 13:59:22 +0800

    feat: add dental clinic user service

--- app/Services/DentalClinicService.php
+++ app/Services/DentalClinicService.php
@@ -1,3 +1,41 @@
 <?php
 
-echo "this is my dental clinic service";
+require_once __DIR__ . '/../Models/Patient.php';
+require_once __DIR__ . '/../Models/Dentist.php';
+require_once __DIR__ . '/../Models/Receptionist.php';
+
+class DentalClinicService
+{
+    public function getDentalClinicUsers(): array
+    {
+        $patient = new Patient(
+            "U001",
+            "Carl Joseph Sumagang",
+            "09171234567",
+            "P001",
+            "Toothache"
+        );
+
+        $dentist = new Dentist(
+            "U002",
+            "Dr. James Ian",
+            "09181234567",
+            "General Dentistry",
+            "DMD-001"
+        );
+
+        $receptionist = new Receptionist(
+            "U003",
+            "John Lou",
+            "09191234567",
+            "EMP-001",
+            "Morning"
+        );
+
+        return [
+            $patient,
+            $dentist,
+            $receptionist
+        ];
+    }
+}
