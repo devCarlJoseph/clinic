f2c1fafc45d760b7c7e397a42aba729deff975f5
devCarlJoseph <carljosephsumagang17@gmail.com>
2026-09-18 13:36:27 +0800

    feat: add Patient class with inheritance

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "this is my patient";
+require_once __DIR__ . '/DentalClinicUser.php';
+
+class Patient extends DentalClinicUser
+{
+    private string $patientNo;
+    private string $dentalConcern;
+
+    public function __construct(string $userId, string $name, string $contactNum, string $patientNo, string $dentalConcern)
+    {
+        parent::__construct($userId, $name, $contactNum);
+
+        $this->patientNo = $patientNo;
+        $this->dentalConcern = $dentalConcern;
+    }
+
+
+    public function getRoleDescription(): string 
+    {
+        return "Patient - receives dental consultation and services";
+    }
+}
