7b14fbd7cd1f364066d92c284890f4c7d41e93b7
devCarlJoseph <carljosephsumagang17@gmail.com>
2026-09-18 13:35:28 +0800

    feat: create base DentalClinicUser class

--- app/Models/DentalClinicUser.php
+++ app/Models/DentalClinicUser.php
@@ -1,3 +1,34 @@
 <?php
 
-echo "this is my dental clinic user";
+
+class DentalClinicUser
+{
+    protected function DentalClinicUser 
+    {
+        protected string $name,
+        protected string $contactNum,
+        private string $userId,
+
+        public function __construct( string $userId, string $contactNum, string $name) 
+        {
+          $this->userId = $userId;
+          $this->name = $name;
+          $this->contactNumber = $contactNum;
+        }
+
+        public function getName(): string 
+        {
+            return $this->name;
+        }
+
+        public function getContactNumber(): string 
+        {
+            return $this->contactNumber;
+        }
+
+        public function getRoleDescription(): string 
+        {
+            return "General dental clinic user";
+        }
+    }
+}
