bba86fbccd109e8b598b14395466ee1870405c9c
devCarlJoseph <carljosephsumagang17@gmail.com>
2026-09-18 13:36:37 +0800

    feat: add Dentist class with inheritance

--- app/Models/Dentist.php
+++ app/Models/Dentist.php
@@ -1,3 +1,24 @@
 <?php
 
-echo "this is my dentist";
+require_once __DIR__ . '/DentalClinicUser.php';
+
+
+class Dentis extends DentalClinicUser
+{
+    protected string $specialization;
+    protected string $licenseNo;
+
+
+    public function __construct(string $userId, string $name, string $contactNum, string $specialization, string $licenseNo)
+    {
+        parent::__construct($userId, $name, $contactNum);
+
+        $this->specialization = $specialization;
+        $this->licenseNo = $licenseNo;
+    }
+
+    public function getRoleDescription(): string
+    {
+        return "Dentist - provides dental consultation and treatment.";
+    }
+}
